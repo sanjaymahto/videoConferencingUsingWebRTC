046d9672a7d4e2b8bd84051573acb5f087201fa2
sanjay <sanjaykumar.m@nference.net>
2022-06-05 21:21:49 +0530

    screen sharing logic implemented

--- videoconferencing/src/RoomPage/VideoSection/MicButton.js
+++ videoconferencing/src/RoomPage/VideoSection/MicButton.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import MicButtonImg from "../../resources/images/mic.svg";
 import MicButtonImgOff from "../../resources/images/micOff.svg";
+import * as webRTCHandler from '../../utils/webRTCHandler'
 
 const MicButton = () => {
   const [isMicMuted, setIsMicMuted] = useState(false);
 
   const handleMicButtonPressed = () => {
+    webRTCHandler.toggleMic(isMicMuted);
     setIsMicMuted(!isMicMuted);
   };
 
   return (
     <div className="video_button_container">
       <img
+        alt="micButton"
         src={isMicMuted ? MicButtonImgOff : MicButtonImg}
         onClick={handleMicButtonPressed}
         className="video_button_image"
--- videoconferencing/src/utils/webRTCHandler.js
+++ videoconferencing/src/utils/webRTCHandler.js
@@ -152,3 +152,44 @@ const addStream = (stream, connUserSocketId) => {
   videoContainer.appendChild(videoElement);
   videosContainer.appendChild(videoContainer);
 };
+
+/////////////////////////////////////////////BUTTONS LOGIC ///////////////////////////////////////////////////////////
+
+export const toggleMic = (ismuted) => {
+  localStream.getAudioTracks()[0].enabled = ismuted ? true : false
+}
+
+export const toggleCameraButton = (isDisabled) => {
+  localStream.getVideoTracks()[0].enabled = isDisabled ? true: false
+}
+
+export const toggleScreenShare = (
+  isScreenSharingActive,
+  screenSharingStream = null
+) => {
+  if (isScreenSharingActive) {
+    switchVideoTracks(localStream);
+  } else {
+    switchVideoTracks(screenSharingStream);
+  }
+};
+
+const switchVideoTracks = (stream) => {
+  for (let socket_id in peers) {
+    for (let index in peers[socket_id].streams[0].getTracks()) {
+      for (let index2 in stream.getTracks()) {
+        if (
+          peers[socket_id].streams[0].getTracks()[index].kind ===
+          stream.getTracks()[index2].kind
+        ) {
+          peers[socket_id].replaceTrack(
+            peers[socket_id].streams[0].getTracks()[index],
+            stream.getTracks()[index2],
+            peers[socket_id].streams[0]
+          );
+          break;
+        }
+      }
+    }
+  }
+};
